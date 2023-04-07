13f38b97b7178f306248f3df6e95fd0528a00239
Gianluca Riggio <luca@maximumawesome.nl>
2023-04-07 22:33:29 +0200

    added basic config

---
+++ config/app.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Plugin Name
+    |--------------------------------------------------------------------------
+    |
+    | This value is the name of your plugin.
+    |
+    */
+    'name' => 'knapsack_plugin',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Editor
+    |--------------------------------------------------------------------------
+    |
+    | This value will be used to determine which editor should be used to open
+    | editor links. Supported values are: vscode, phpstorm, atom, sublime.
+    |
+    */
+    'editor' => 'vscode',
+];

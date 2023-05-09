567cebde3bfa195d5ce39526b7ff00cc875e1fb1
Gianluca Riggio <luca@maximumawesome.nl>
2023-05-09 20:19:16 +0200

    Added a scoped preflight to prevent messing up wordpress css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  important: '.tailwind',
   content: ['./index.php', './resources/**/*.blade.php', './resources/**/*.ts'],
   theme: {
     extend: {},
   },
   plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
 }

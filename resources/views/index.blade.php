252bbcab85953e37ec079be5f8008c4c1eb52ec0
Gianluca Riggio <luca@maximumawesome.nl>
2023-05-09 20:23:17 +0200

    added example form

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,24 @@
 @extends('layouts.public')
 
 @section('content')
-    <main class="h-screen w-screen bg-slate-900 text-slate-100">
-        <h1>Plugin main view</h1>
-    </main>
+    <div class="tailwind">
+        <main class="m-10 bg-white p-6 shadow-sm rounded-sm">
+            <header>
+                <h1 class="text-xl">{{ __('Plugin main view') }}</h1>
+            </header>
+            <form class="my-4" method="post">
+                <input
+                    type="text"
+                    name="name"
+                    placeholder="name"
+                    class="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
+
+                <input
+                    type="submit"
+                    value="Submit"
+                    class="mt-2 rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+            </form>
+        </div>
+    </div>
 @endsection

62543af7deb62ddc6b470b8196a22fef0f67727e
Gianluca Riggio <luca@maximumawesome.nl>
2023-05-09 20:23:04 +0200

    added API route example

--- plugin.php
+++ plugin.php
@@ -1,5 +1,9 @@
 <?php
 
+if (! defined('ABSPATH')) {
+    exit; // disable direct access
+}
+
 /*
 Plugin Name: Knapsack plugin boilerplate
 Description: A plugin boilerplate for WordPress
@@ -16,4 +20,29 @@ Domain Path: /lang
 | Bootstrap the plugin
 |--------------------------------------------------------------------------
 */
+
 require_once __DIR__.'/bootstrap/app.php';
+
+// // Adding an API endpoint
+// add_action('rest_api_init', 'register_my_api_endpoint');
+
+// function register_my_api_endpoint()
+// {
+//     register_rest_route('my-plugin/v1', '/my-endpoint', [
+//         'methods' => 'GET',
+//         'callback' => 'my_api_endpoint_handler',
+//         'permission_callback' => function () {
+//             return true;
+//             // return current_user_can( 'edit_others_posts' );
+//         },
+//     ]);
+// }
+
+// // And API route registers a namespace, a route and a callback.
+
+// function my_api_endpoint_handler($request)
+// {
+//     $data = ['message' => 'Hello Plugin!'];
+
+//     return new WP_REST_Response($data, 200);
+// }

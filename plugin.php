fa44661c62d17da326a8ed912096c4f5d6e47f6e
Gianluca Riggio <luca@maximumawesome.nl>
2023-05-12 13:39:18 +0200

    removed comments

--- plugin.php
+++ plugin.php
@@ -22,27 +22,3 @@ Domain Path: /lang
 */
 
 require_once __DIR__.'/bootstrap/app.php';
-
-// // Adding an API endpoint
-// add_action('rest_api_init', 'register_my_api_endpoint');
-
-// function register_my_api_endpoint()
-// {
-//     register_rest_route('my-plugin/v1', '/my-endpoint', [
-//         'methods' => 'GET',
-//         'callback' => 'my_api_endpoint_handler',
-//         'permission_callback' => function () {
-//             return true;
-//             // return current_user_can( 'edit_others_posts' );
-//         },
-//     ]);
-// }
-
-// // And API route registers a namespace, a route and a callback.
-
-// function my_api_endpoint_handler($request)
-// {
-//     $data = ['message' => 'Hello Plugin!'];
-
-//     return new WP_REST_Response($data, 200);
-// }

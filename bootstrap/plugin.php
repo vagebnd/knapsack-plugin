f479d23912cfa17eb9b5abd6203e3c40d588e60e
Tom Huynen <tom@maximumawesome.nl>
2023-05-19 11:22:27 +0200

    register bootstrappers

--- bootstrap/plugin.php
+++ bootstrap/plugin.php
@@ -1,5 +1,7 @@
 <?php
 
 require_once __DIR__.'/plugin/assets.php';
-require_once __DIR__.'/plugin/models.php';
+require_once __DIR__.'/plugin/custom-post-types.php';
+require_once __DIR__.'/plugin/taxonomies.php';
 require_once __DIR__.'/plugin/shortcodes.php';
+require_once __DIR__.'/plugin/blocks.php';

61099e7538048e2b9edaf8144785d3955d3563d5
Tom Huynen <tom@maximumawesome.nl>
2023-06-01 15:13:06 +0200

    register controls

--- elementor/Factory.php
+++ elementor/Factory.php
@@ -9,6 +9,27 @@ class Factory
 {
     public function __construct()
     {
+        $this->registerControls();
+        $this->registerWidgets();
+    }
+
+    private function registerControls()
+    {
+        add_action('elementor/controls/register', function ($manager) {
+            $finder = Finder::create()
+                ->in(path('elementor/controls'))
+                ->files();
+
+            foreach ($finder as $file) {
+                require_once $file->getRealPath();
+                $Control = 'Skeleton\\Elementor\\Controls\\' . $file->getBasename('.php');
+                $manager->register(new $Control());
+            }
+        });
+    }
+
+    private function registerWidgets()
+    {
         add_action('elementor/widgets/register', function ($manager) {
             $finder = Finder::create()
                 ->in(path('elementor/widgets'))
@@ -16,7 +37,7 @@ class Factory
 
             foreach ($finder as $file) {
                 require_once $file->getRealPath();
-                $Widget = 'Skeleton\\Elementor\\' . $file->getBasename('.php');
+                $Widget = 'Skeleton\\Elementor\\Widgets\\' . $file->getBasename('.php');
                 $manager->register(new $Widget());
             }
         });

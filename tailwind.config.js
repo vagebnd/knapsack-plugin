61ba1e606a5c58b1a387330ebc3db817d1b74f54
Tom Huynen <tom@maximumawesome.nl>
2023-06-05 15:10:45 +0200

    add colors, remove theme plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,22 @@ module.exports = {
   important: '.tailwind',
   content: ['./index.php', './resources/**/*.blade.php', './resources/**/*.ts', './resources/**/*.vue'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'zinc-0': '#080809',
+        'zinc-100': '#121415',
+        'zinc-200': '#1D2022',
+        'zinc-300': '#2A2E31',
+        'zinc-400': '#36383E',
+        'zinc-500': '#41464C',
+        'zinc-600': '#4C5359',
+        'zinc-700': '#586067',
+        'zinc-800': '#646C75',
+        'zinc-900': '#6F7883',
+      },
+    },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-  ],
+  plugins: [require('@tailwindcss/forms')],
   corePlugins: {
     preflight: false,
   },

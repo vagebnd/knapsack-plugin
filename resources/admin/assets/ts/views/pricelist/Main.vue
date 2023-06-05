4a6a2b5ed89168324f893242a9ec559ecf65494f
Tom Huynen <tom@maximumawesome.nl>
2023-06-05 15:10:58 +0200

    remove pricelist admin page

--- resources/admin/assets/ts/views/pricelist/Main.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="pricelist tailwind">
-    <router-view></router-view>
-  </div>
-</template>
--- routes/admin.php
+++ routes/admin.php
@@ -1,11 +1,1 @@
 <?php
-
-use Knapsack\Compass\Support\Facades\Route;
-use Skeleton\Http\Controllers\AdminPages\PriceListController;
-use function Skeleton\Support\resource_url;
-
-Route::adminPage('/pricelist', function ($page) {
-    $page->get('/', [PriceListController::class, 'index']);
-}, [
-    'icon' => resource_url('admin/assets/img/icon.svg'),
-]);

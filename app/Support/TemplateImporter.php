692d674292c3a5b64c4b680c219e877ee944a41e
Tom Huynen <tom@maximumawesome.nl>
2023-08-17 16:50:08 +0200

    import

--- app/Support/TemplateImporter.php
+++ app/Support/TemplateImporter.php
@@ -13,6 +13,8 @@ class TemplateImporter
 {
     private Filesystem $fileSystem;
 
+    private $importedPosts = [];
+
     public function __construct()
     {
         global $wp_filesystem;
@@ -28,33 +30,136 @@ class TemplateImporter
     private function importData($path)
     {
         $dataPath = implode(DIRECTORY_SEPARATOR, [$path, 'data.json']);
+
         $data = $this->fileSystem->get($dataPath);
         $data = $this->parseData($data);
         $data = json_decode($data, true);
 
-        foreach (Arr::get($data, 'posts') as $post) {
-            $postID = wp_insert_post(Arr::only($post, [
-                'post_title',
-                'post_content',
-                'post_name',
-                'post_type',
-                'post_status',
-                'post_author',
-                'menu_order',
-                'guid',
-                'post_parent',
-            ]));
+        $posts = Arr::get($data, 'posts', []);
+        $terms = Arr::get($data, 'terms', []);
+        $navigations = Arr::get($data, 'navigations', []);
 
-            foreach (Arr::get($post, 'meta') as $key => $value) {
-                $value = array_shift($value);
+        $this->clean();
+        $this->importTerms($terms);
+        $this->importPosts($posts);
+        $this->importNavigations($navigations);
+    }
 
-                if (is_serialized($value)) {
-                    $value = unserialize($value);
+    private function clean()
+    {
+        collect(get_theme_mods())
+            ->each(function ($option) {
+                delete_option($option);
+            });
+
+        // TODO:: remove this later on.
+        $posts = collect(get_posts([
+            'post_type' => ['page', 'post', 'pricelistsection', 'pricelistitem', 'pricelistitem', 'pricelist', 'nav_menu_item'],
+            'post_status' => 'any',
+            'posts_per_page' => -1,
+        ]));
+
+        $posts->each(function ($post) {
+            wp_delete_post($post->ID, true);
+        });
+    }
+
+    private function importPosts($posts)
+    {
+        $this->importedPosts = collect($posts)
+            ->map(function ($post) {
+                return Arr::forget($post, 'ID');
+            })
+            ->mapWithKeys(function ($post) {
+                $postID = wp_insert_post($post);
+                $post['ID'] = $postID;
+                return [$postID => $post];
+            });
+    }
+
+    private function importTerms($terms)
+    {
+        $currentTerms = collect(get_terms());
+
+        collect($terms)
+            ->filter(function ($term) use ($currentTerms) {
+                return ! $currentTerms->contains(function ($currentTerm) use ($term) {
+                    return $currentTerm->slug === $term['slug'] && $currentTerm->taxonomy === $term['taxonomy'];
+                });
+            })
+            ->each(function ($term) {
+                wp_insert_term($term['name'], $term['taxonomy'], [
+                    'slug' => $term['slug'],
+                ]);
+            });
+    }
+
+    private function importNavigations($navigations)
+    {
+        collect($navigations)
+            ->each(function ($navigation) {
+                $name = Arr::get($navigation, 'name');
+                $slug = Arr::get($navigation, 'slug');
+                $items = Arr::get($navigation, 'items', []);
+                $location = Arr::get($navigation, 'location');
+
+                if (term_exists($slug, 'nav_menu')) {
+                    $term = get_term_by('slug', $slug, 'nav_menu');
+                } else {
+                    $term = wp_insert_term($name, 'nav_menu', [
+                        'slug' => $slug,
+                    ]);
                 }
 
-                add_post_meta($postID, $key, $value);
-            }
-        }
+                $locations = get_theme_mod('nav_menu_locations', []);
+                $locations[$location] = $term->term_id;
+                set_theme_mod('nav_menu_locations', $locations);
+
+                $this->importMenuItems($items, $term, 0);
+            });
+    }
+
+    private function importMenuItems($items, $term, $parentID)
+    {
+        collect($items)
+            ->each(function ($item) use ($term, $parentID) {
+                $post = $this->getPostBySlug($item['postSlug']);
+
+                $navItemPostID = wp_insert_post([
+                    'post_title' => $item['title'],
+                    'guid' => $item['url'],
+                    'post_type' => 'nav_menu_item',
+                    'post_status' => 'publish',
+                    'comment_status' => 'closed',
+                    'ping_status' => 'closed',
+                    'meta_input' => [
+                        '_menu_item_type' => 'post_type',
+                        '_menu_item_menu_item_parent' => $parentID,
+                        '_menu_item_object_id' => $post['ID'],
+                        '_menu_item_object' => 'page',
+                        '_menu_item_target' => '',
+                        '_menu_item_classes' => 'a:1:{i:0;s:0:"";}',
+                        '_menu_item_xfn' => '',
+                        '_menu_item_url' => '',
+                    ],
+                ]);
+
+                wp_set_object_terms($navItemPostID, $term->term_id, 'nav_menu');
+
+                $items = Arr::get($item, 'items', []);
+
+                if (count($items) > 0) {
+                    $this->importMenuItems($items, $term, $navItemPostID);
+                }
+            });
+    }
+
+    private function getPostBySlug($slug)
+    {
+        return collect($this->importedPosts)
+            ->first(function ($post) use ($slug) {
+                return $post['post_name'] === $slug;
+            });
     }
 
     private function parseData($data)
